b66f4d2c272f1583b77cad5cc73ebd46ee8611ac
sahilkhan03 <sahilkhan0307.sk@gmail.com>
2020-12-11 13:27:48 +0530

    changes

--- index.js
+++ index.js
@@ -3,28 +3,37 @@ const app = express();
 const cors = require("cors")
 const fileUpload = require("express-fileupload")
 const PORT = process.env.PORT || 4000;
-const {PythonShell} = require('python-shell')
+const { PythonShell } = require('python-shell')
 
 app.use(express.static(__dirname + '/public'));
 app.use(cors());
 app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : '/tmp/'
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
 }))
 
-app.post('/api/submitdata', function(req, res) {
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+app.post('/api/submitdata', function (req, res) {
     let options = {
         mode: 'text',
-        pythonOptions: ['-u'], 
-        scriptPath: __dirname, 
-        args: [req.files.districtData.tempFilePath, req.files.labData.tempFilePath] 
+        pythonOptions: ['-u'],
+        scriptPath: __dirname,
+        args: [req.files.districtData.tempFilePath, req.files.labData.tempFilePath]
     };
-    PythonShell.run('script.py', options, function (err, result) {
-        if (err) throw err;
-        res.sendFile(__dirname + '/output.json')
-    }); 
+    // PythonShell.run('script.py', options, function (err, result) {
+    //     if (err) throw err;
+    //     res.sendFile(__dirname + '/output.json')
+    // }); 
+    res.send('hello')
 })
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log(`Server started on ${PORT}`);
 })
